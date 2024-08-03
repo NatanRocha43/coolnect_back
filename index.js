aa50b1e09ff1435e2b77196d190a11cb30d89b56
ViniciusPereira03 <vinicius.gpereira@fatecitapetiniga.edu.br>
2024-08-03 16:24:36 -0300

    Add lib cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // index.js
 const express = require('express');
+var cors = require('cors')
 const routes = require('./src/routes')
 
 const app = express();
-const port = 3000;
+const port = 3030;
 
+app.use(cors())
 app.use(express.json());
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}/`);
