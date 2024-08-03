3a1a08dd1f4c8fee7bc69c00159fa70bb1e9ef30
ViniciusPereira03 <vinicius.gpereira@fatecitapetiniga.edu.br>
2024-08-03 15:20:24 -0300

    Criado cadastro de eventos e curtidas de eventos

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const authMiddleware = require('../middleware/auth');
+
 const userService = require('../services/user');
+const eventService = require('../services/event');
 
 const router = express.Router();
 
 router.post('/cadastro', userService.add)
 router.post('/login', userService.login)
 router.post('/senha', userService.senha)
+
+router.post('/evento', authMiddleware, eventService.add)
+router.post('/evento/curtir', authMiddleware, eventService.curtir)
 
 module.exports = router;
---
+++ src/services/event.js
@@ -0,0 +1,54 @@
+const Eventos = require('../models/Eventos');
+const connectDB = require('../config/database');
+
+connectDB();
+
+eventService = {}
+
+eventService.add = async (req, res) => {
+  try {
+    const newEvent = new Eventos(req.body);
+    const result = await newEvent.save();
+  
+    return res.status(200).json(result);
+
+  } catch (error) {
+    return res.status(422).json({
+      message: "Erro no cadastro de evento",
+      error
+    })
+  }
+}
+
+eventService.curtir = async (req, res) => {
+  try {
+    const data = req.body;
+    
+    const evento = await Eventos.findOne({
+      _id: data.id
+    });
+
+    if (!evento) {
+      return res.status(422).json({
+        message: "Evento não encontrado"
+      });
+    }
+
+    let curtidas = evento.curtidas;
+    curtidas = curtidas + 1;
+    const curtidasAtualizadas = curtidas;
+
+    await Eventos.findOneAndUpdate({ _id: evento.id }, { curtidas: curtidasAtualizadas });
+
+    return res.status(200).json({
+      message: "Evento curtido com sucesso!"
+    })
+  } catch (error) {
+    return res.status(422).json({
+      message: "Erro inesperado para curtir evento!",
+      error
+    })
+  }
+}
+
+module.exports = eventService
