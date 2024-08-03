ebe9a40715d506ebd58a84c4bad602153fc90b94
ViniciusPereira03 <vinicius.gpereira@fatecitapetiniga.edu.br>
2024-08-03 14:15:42 -0300

    Criado cadastro de usuário

--- src/services/user.js
+++ src/services/user.js
@@ -2,6 +2,20 @@ userService = {}
 
 userService.add = async (req, res) => {
 
+  try {
+    const newUser = new Usuarios(req.body);
+    const result = await newUser.save();
+  
+    return res.status(200).json(result);
+
+  } catch (error) {
+    return res.status(422).json({
+      message: "Erro no cadastro de usuário",
+      error
+    })
+  }
+}
+
   return res.status(200).json({
     user: req.body
   });
